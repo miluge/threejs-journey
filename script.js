3de5c52256c6619c3b8ee6169df4525da2bad70d
miluge <gllmblndl@gmail.com>
2022-04-14 08:01:50 +0200

    add mesh to scene

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello Three')
+console.log(THREE)
+
+// Scene
+const scene = new THREE.scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+// Add mesh to scene
+scene.add(mesh)
